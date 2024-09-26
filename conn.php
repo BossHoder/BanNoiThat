1c0ed3f5f400a39567906b9871cb1060f4741e47
BossHoder <thetruong275@gmail.com>
2024-09-26 07:38:00 +0700

    add connector

---
+++ conn.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";  // Tên máy chủ MySQL (thường là localhost)
+$username = "root";         // Tên người dùng MySQL
+$password = "azz123123";             // Mật khẩu của người dùng MySQL
+$dbname = "QLBH";   // Tên database bạn muốn kết nối
+
+// Tạo kết nối
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+    die("Kết nối thất bại: " . $conn->connect_error . "");
+}
+echo "Kết nối thành công ";
